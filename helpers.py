d146bfeab407c4f4bee19e3e9dfb64aa4fa369e5
PERRIN <arthur.perrin@e-i.fr>
2021-09-03 15:53:58 +0200

    ganhacks simplification

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 import matplotlib
 import numpy as np
+import logging as lg
 import torch.utils.data as dt
 import matplotlib.pyplot as plt
 
@@ -7,37 +8,46 @@ import constants as cst
 import modules.database as database
 import modules.neuralnetwork as neuralnetwork
 
-matplotlib.use('Qt5Agg')
-plt.style.use('ggplot')
+matplotlib.use("Qt5Agg")
+plt.style.use("ggplot")
+
 
 def get_polynomial_function(coefs: list):
-    deg = len(coefs)-1
-    return lambda x:sum([coef*x**(deg-i) for (i, coef) in enumerate(coefs)])
+    deg = len(coefs) - 1
+    return lambda x: sum([coef * x ** (deg - i) for (i, coef) in enumerate(coefs)])
+
 
 def sample_data(num: int, gen, scale: float = 100) -> float:
-    data = []
-    X = scale*(np.random.random_sample((num,))-0.5)
+    X = scale * (np.random.random_sample((num,)) - 0.5)
     for x in X:
         yield (x, gen(x))
+
 
 def plot_data(data: list) -> None:
     data = np.array(data)
     _, ax = plt.subplots()
-    ax.scatter(data[:, 0], data[:, 1], color='blue', alpha=.5)
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
+    ax.scatter(data[:, 0], data[:, 1], color="blue", alpha=0.5)
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
     return ax
 
 
 def main():
-    """ FOR TESTING PURPOSES """
+    """FOR TESTING PURPOSES"""
     POLY = cst.POLY
     gen = get_polynomial_function(POLY)
-    sampler = sample_data(100, gen, scale=1)
-    # ax = plot_data(list(sampler))
-    sampler = sample_data(100, gen, scale=1)
+    sampler = sample_data(1000, gen, scale=1)
     dataset = dt.DataLoader(database.Samples(sampler), batch_size=128)
-    neuralnetwork.train_gan(dataset, neuralnetwork.Generator(10, 2), neuralnetwork.Discriminator(2, 2), epochs=100001, lr=cst.LEARNING_RATE) # write_graph=True, n_test_graph=100)
+    neuralnetwork.train_gan(
+        dataset,
+        neuralnetwork.Generator(2, 2),
+        neuralnetwork.Discriminator(2, 2),
+        epochs=100001,
+        lr=cst.LEARNING_RATE,
+        display_test=True,
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    lg.root.setLevel(lg.INFO)
     main()
--- modules/neuralnetwork.py
+++ modules/neuralnetwork.py
@@ -1,60 +1,74 @@
 import torch
+import logging as lg
 from torch import nn
 import matplotlib.pyplot as plt
 from torch.nn.modules.loss import BCEWithLogitsLoss
 
-class Generator(nn.Module):
-    def __init__(self, input_size:int, output_size:int, hidden_sizes:list=[16, 16], relu_slope:float=.01):
-        super(Generator, self).__init__()
-        self.input_size = input_size # save the input size
 
-        dense_layers = [] # list of nn.Modules to be used
-        for h in hidden_sizes: # build the list given the different hidden layers sizes
+class Generator(nn.Module):
+    def __init__(
+        self,
+        input_size: int,
+        output_size: int,
+        hidden_sizes: list = [16, 16],
+        relu_slope: float = 0.01,
+    ):
+        super(Generator, self).__init__()
+        self.input_size = input_size  # save the input size
+
+        dense_layers = []  # list of nn.Modules to be used
+        for h in hidden_sizes:  # build the list given the different hidden layers sizes
             dense = nn.Linear(in_features=input_size, out_features=h)
             activation = nn.LeakyReLU(negative_slope=relu_slope)
             dense_layers.append(dense)
             dense_layers.append(activation)
-            input_size = h # update the input_size for the next dense layer
+            input_size = h  # update the input_size for the next dense layer
         # unpack the list in a Sequential object
         self.dense_stack = nn.Sequential(*dense_layers)
         # build the ouput layer
         self.output = nn.Linear(in_features=input_size, out_features=output_size)
-    
+
     def forward(self, x):
         features = self.dense_stack(x)
         y = self.output(features)
         return y
 
+
 class Discriminator(nn.Module):
-    def __init__(self, input_size:int, feature_size:int, hidden_sizes:list=[16, 16], relu_slope:float=.01):
+    def __init__(
+        self,
+        input_size: int,
+        feature_size: int,
+        hidden_sizes: list = [16, 16],
+        relu_slope: float = 0.01,
+    ):
         super(Discriminator, self).__init__()
-        dense_layers = [] # list of nn.Modules to be used
-        for h in hidden_sizes: # build the list given the different hidden layers sizes
+        dense_layers = []  # list of nn.Modules to be used
+        for h in hidden_sizes:  # build the list given the different hidden layers sizes
             dense = nn.Linear(in_features=input_size, out_features=h)
             activation = nn.LeakyReLU(negative_slope=relu_slope)
             dense_layers.append(dense)
             dense_layers.append(activation)
-            input_size = h # update the input_size for the next dense layer
+            input_size = h  # update the input_size for the next dense layer
         # add the feature output layer
-        dense_layers.append(nn.Linear(in_features=input_size, out_features=feature_size))
+        dense_layers.append(
+            nn.Linear(in_features=input_size, out_features=feature_size)
+        )
         # unpack the list in a Sequential object
         self.dense_stack = nn.Sequential(*dense_layers)
         # build the output layer
         self.output = nn.Linear(in_features=feature_size, out_features=1)
-    
+
     def forward(self, x):
         features = self.dense_stack(x)
         logit = self.output(features)
         return logit, features
 
-def gen_loss(target, output):
-    loss = BCEWithLogitsLoss()(output, target)
-    return loss
 
-def disc_loss(real_target, real_output, fake_target, fake_output):
-    loss_real = BCEWithLogitsLoss()(real_output, real_target)
-    loss_fake = BCEWithLogitsLoss()(fake_output, fake_target)
-    return loss_real + loss_fake
+def loss(target, output):
+    loss_ = BCEWithLogitsLoss()(output, target)
+    return loss_
+
 
 def train_loop(dataloader, generator, discriminator, lr=1e-3):
     gen_input_size = generator.input_size
@@ -70,48 +84,102 @@ def train_loop(dataloader, generator, discriminator, lr=1e-3):
         labels_real = torch.ones([T.shape[0], 1])
         # train the discriminator
         dis_optimizer.zero_grad()
+        # (1) with real data
         preds_real, _ = discriminator(T.float())
-        preds_fake, _ = discriminator(generated.float())
-        dloss = disc_loss(labels_real, preds_real, labels_fake, preds_fake)
-        dloss.backward()
+        dloss_real = loss(labels_real, preds_real)
+        dloss_real.backward()
+        # (2) with fake data
+        preds_fake, _ = discriminator(generated.detach())
+        dloss_fake = loss(labels_fake, preds_fake)
+        dloss_fake.backward()
+        # update the loss and discriminator
+        dloss = dloss_real + dloss_fake
         dis_optimizer.step()
         # train the generator
         gen_optimizer.zero_grad()
-        noise = torch.randn([T.shape[0], gen_input_size])
-        preds, _ = discriminator(generator(noise.float()))
-        gloss = gen_loss(labels_real, preds) # fooling the discriminator with 'real' labels to compute generator's loss
+        preds, _ = discriminator(generated)
+        gloss = loss(
+            labels_real, preds
+        )  # fooling the discriminator with 'real' labels to compute generator's loss
         gloss.backward()
         gen_optimizer.step()
 
     discloss = dloss.item()
     genloss = gloss.item()
-    
+
     return discloss, genloss
 
-def write_test_graph(generator, noise_bench, title=None):
+
+def write_test_graph(generator, noise_bench, title=None, overlay=None):
     with torch.no_grad():
         preds = generator(noise_bench).numpy()
         plt.figure()
-        plt.scatter(preds[:, 0], preds[:, 1], color='red', marker='+')
+        plt.scatter(preds[:, 0], preds[:, 1], color="red", marker="+")
+        if overlay:
+            plt.scatter(overlay[0], overlay[1], color="blue", marker=".", alpha=0.5)
         if not title:
-            plt.savefig(f'output.png')
+            plt.savefig(f"output.png")
         else:
             plt.savefig(title)
 
-def train_gan(dataloader, generator, discriminator, lr=1e-3, epochs=100, write_graph:bool=False, n_test_graph:int=200):
+
+def train_gan(
+    dataloader,
+    generator,
+    discriminator,
+    lr=1e-3,
+    epochs=100,
+    write_graph: bool = False,
+    n_test_graph: int = 200,
+    display_test: bool = False,
+):
     dloss, gloss = [], []
     gen_input_size = generator.input_size
     noise_bench = torch.randn([n_test_graph, gen_input_size])
-    for t in range(epochs):
-        d, g = train_loop(dataloader, generator, discriminator, lr=lr)
-        print(f'Epoch {t} -- Disc loss: {d:>7f} | Gen loss: {g:>7f}', end='\r', flush=True)
-        dloss.append(d)
-        gloss.append(g)
-        if write_graph:
-            if epochs<=10 or t % (epochs//5):
-                title = f'epoch_{t}.png'
-                write_test_graph(generator, noise_bench, title)
-        if g<.0001 or abs(d-g)<0.001:
-            write_test_graph(generator, noise_bench)
-            break
-    print('')
+    try:
+        lg.info("Beginning training...")
+        for t in range(epochs):
+            d, g = train_loop(dataloader, generator, discriminator, lr=lr)
+            print(
+                f"Epoch {t} -- Disc loss: {d:>7f} | Gen loss: {g:>7f}",
+                end="\r",
+                flush=True,
+            )
+            dloss.append(d)
+            gloss.append(g)
+            if write_graph:
+                if epochs <= 10 or t % (epochs // 10):
+                    title = f"./output/epoch_{t}.png"
+                    write_test_graph(generator, noise_bench, title)
+            if g < 0.0001 or d < 0.0001 or abs(d - g) < 0.001:
+                print("")
+                lg.info(f"Training stopped after epoch {t+1}...")
+                if write_graph:
+                    write_test_graph(
+                        generator, noise_bench, title=f"./output/epoch_{t}(last).png"
+                    )
+                break
+        print("")
+    except KeyboardInterrupt:
+        print("")
+        lg.warning(f"Training interrupted at epoch {t}!")
+
+    lg.info("Plotting training info...")
+    plt.figure()
+    plt.plot(dloss, label="Discriminator")
+    plt.plot(gloss, label="Generator")
+    plt.title("Loss per epoch")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss (BCE with logits)")
+    plt.savefig("./output/training_graph.png")
+    if display_test:
+        lg.info("Generating a test set...")
+        dataset = dataloader.dataset
+        data = [dataset.__getitem__(i).view(1, -1) for i in range(len(dataset))]
+        data = torch.cat(data, dim=0).numpy()
+        write_test_graph(
+            generator,
+            noise_bench,
+            title="./output/display_test_set.png",
+            overlay=(data[:, 0], data[:, 1]),
+        )
